e237d7f60e655f06ee99a4293e5f301f3b778de8
Choi Yeongjun <frozenmoai@gmail.com>
2019-06-22 23:34:36 +0900

    Updated a lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,13 +1,16 @@
-import json
+import os
+import json 
 import requests
 
-INCOMING_URL = os.getenv('INCOMING_URL')
 
 def lambda_handler(event, context):
-    r = requests.post( INCOMING_URL, json={'text' : 'test'})
+    clickType=event['clickType']
+    message = os.getenv('{}_CLICK'format(clickType))
+    r = send_notify(message)
     return json.dumps(r.text)
+    
 
-
-
-
-
+def send_notify_to_slack(message):
+    INCOMING_URL = os.getenv('INCOMING_URL')
+    r = requests.post(INCOMING_URL, json={'text' : message})
+    return r
